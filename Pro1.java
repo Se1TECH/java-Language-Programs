2f243fb29aedda6b92553f636643a398c9002e43
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-16 21:33:23 +0530

    Create Pro1.java

---
+++ Pro1.java
@@ -0,0 +1,11 @@
+//Question: Write Program in java to Print any String Value Enter by User.
+import java.util.*;
+class Pro1{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your String: ");
+        String str= sc.nextLine();
+        System.out.println("Your String value is :- " + str);
+    } 
+}
