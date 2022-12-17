60877c319eb967c1516b94da6c1f145f1682308f
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-17 17:14:37 +0530

    Create Pro4.java

---
+++ Pro4.java
@@ -0,0 +1,21 @@
+// Question: Write program in Java To Swap Two Number.
+import java.util.*;
+class Pro4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of N1 : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter Value of N2 : ");
+        int n2 = sc.nextInt();
+        
+        //Swap Two Numbers
+        int n3 = n1;
+        n1 = n2;
+        n2 = n3;
+
+        System.out.println("N1 Value after Swaping : " + n1);
+        System.out.println("N2 Value after Swaping : " + n2);
+
+
+    }
+}
