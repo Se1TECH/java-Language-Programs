53ab3ad65caffee97aeb6650ddcfb8c2d232d8ec
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-18 19:37:03 +0530

    Create Pro5.java

---
+++ Pro5.java
@@ -0,0 +1,18 @@
+//Question: Write a Program in JAVA to Check User Entered Number is Odd or Even.
+import java.util.*;
+class Pro5{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int number = scan.nextInt();
+        if(number % 2 == 0){
+            System.out.println(number + " is Even");
+        }
+        else if(number == 0){
+            System.out.println(number + " is Zero");
+        }
+        else{
+            System.out.println(number + " is Odd");
+        }
+    }
+}
