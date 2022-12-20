4254a8c3e86b6ccfc23da4e86237de10e8f599d0
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-20 15:38:25 +0530

    Create Pro9.java

---
+++ Pro9.java
@@ -0,0 +1,16 @@
+//Question: Write Progarm in Java to Print Sum of Natural Numbers
+import java.util.*;
+
+class Pro9{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter value for Natural number : ");
+        int number = scan.nextInt();
+        int sum=0;
+        for(int i=1; i<=number;i++){
+            sum+=i;
+        }
+
+        System.out.println("Sum of Natural Numbers : " + sum);
+    }
+}
