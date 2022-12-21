a685ea1a2c2a908a9fea31240e0b601b28259882
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-21 19:10:30 +0530

    Create Pro12.java

---
+++ Pro12.java
@@ -0,0 +1,16 @@
+//Question: Write a Program in Java to Compute the Sum of the Digits of an Integer.
+import java.util.*;
+class Pro12{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int number = scan.nextInt();
+        
+        int sum=0;
+        while(number != 0){
+            sum+=number%10;
+            number/=10;
+        }
+        System.out.println("The Sum of all Digit is : " + sum);
+    }
+}
