e7c0d3cdd0588e3e586ffff03e42b50adc313652
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-26 10:51:53 +0530

    Create Pro17.java

---
+++ Pro17.java
@@ -0,0 +1,23 @@
+// Question: Write a Program in Java to check positive or negative number
+import java.util.*;
+class Pro17{
+
+    static void checkNumber(int number){
+        if(number < 0){
+            System.out.println(number + " is Negative");
+        }
+        else if(number >0){
+            System.out.println(number  + " is Positive");
+        }
+        else{
+            System.out.println(number + " is Zero");
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your Number  : ");
+        int number  = scan.nextInt();
+        checkNumber(number);
+    }
+}
