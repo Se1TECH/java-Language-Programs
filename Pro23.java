bdcecfc96befbd852e3b61e8064d4923f2ba7dbb
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-29 10:12:47 +0530

    Create Pro23.java

---
+++ Pro23.java
@@ -0,0 +1,19 @@
+
+// Write a Program in Java to print the elements of an array present on even position.
+import java.util.*;
+
+class Pro23 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter value of 5 elements : ");
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println("\n\nElements value at even position ");
+        for (int i = 1; i < arr.length; i += 2) {
+            System.out.println(arr[i]);
+        }
+    }
+}
