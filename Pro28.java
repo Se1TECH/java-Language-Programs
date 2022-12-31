981bca452442a5f47d93a945cc5a0947a65bf746
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-31 15:20:48 +0530

    Create Pro28.java

---
+++ Pro28.java
@@ -0,0 +1,16 @@
+// Question: Write a Java Program to count the total number of characters in a string.
+import java.util.*;
+class Pro28{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your String : ");
+        String str = scan.nextLine();
+        int count=0;
+        for(int i=0; i< str.length();i++){
+            if(str.charAt(i) != ' '){
+                count++;
+            }
+        }
+        System.out.println("The Number of Characters in Strings are: " + count);
+    }
+}
