c285663b9008d20620c796c692640c908816a958
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-01-02 06:40:01 +0530

    Create Pro32.java

---
+++ Pro32.java
@@ -0,0 +1,31 @@
+// Question: Write a Java Program to print employee class details.
+import java.util.*;
+class Employee{
+    int employeeId;
+    String employeeName;
+    double employeeSalary;
+
+    Employee()  //Constructor
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Employee ID: ");
+        employeeId = scan.nextInt();
+        System.out.print("Enter Employee Name: ");
+        employeeName = scan.next();
+        System.out.print("Enter Employee Salary: ");
+        employeeSalary = scan.nextDouble();
+    }
+
+    void display(){
+        System.out.println("Employee ID : "  + employeeId);
+        System.out.println("Employee Name : "  + employeeName);
+        System.out.println("Employee Salary : "  + employeeSalary);
+    }
+}
+
+class Pro32{
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(); //Create object of Employee class
+        emp1.display();
+    }
+}
