ef64410ab1a34e35cf36bf10668b2974acdb658e
7Tech <119029097+Se1TECH@users.noreply.github.com>
2023-01-05 06:26:49 +0530

    Create Pro38.java

---
+++ Pro38.java
@@ -0,0 +1,20 @@
+// Question: Write a java program to draw pattern. 
+import java.util.*;
+class Pro38{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter numbe of rows to print the pattern: ");
+        int rows = scan.nextInt();
+
+        for(int i=1; i <= rows; i++){
+            for(int j = 4; j >= i; j--){
+                System.out.print("*");
+            }
+            for(int k = 1 ; k <=i ; k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+        
+    }
+}
